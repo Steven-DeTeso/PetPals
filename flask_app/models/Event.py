5ee7a9abb88ec26d34e9acf50c1b6bc759e80cd0
Russell Valencia <russyvsc@gmail.com>
2022-12-19 08:56:48 -0800

    first models commit, russell

---
+++ flask_app/models/Event.py
@@ -0,0 +1,124 @@
+from sqlite3 import connect
+from flask_app import app
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+from flask_bcrypt import Bcrypt
+from flask_app.models import User
+from datetime import date
+import re
+
+bcrypt = Bcrypt(app)
+
+db = "PetsOnly"
+
+class Event:
+    def __init__(self, data:dict):
+        self.id = data['event_id']
+        self.name = data['name']
+        self.date = data['date']
+        self.time = data['time']
+        self.location = data['location']
+        self.user_id = data['user_id']
+        self.details = data['details']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        self.user = None
+
+    @classmethod
+    def get_all_events(cls):
+        query = """SELECT events.id as event_id, 
+        events.created_at, 
+        events.updated_at, 
+        name, 
+        date,
+        time, 
+        location, 
+        users.id as user_id, 
+        details, 
+        first_name, 
+        last_name, 
+        email, 
+        password, 
+        users.created_at as uc, 
+        users.updated_at as uu 
+        FROM events 
+        LEFT JOIN users_events 
+        ON events.id = users_events.event_id 
+        LEFT JOIN users 
+        ON users.id = users_events.user_id;"""
+        results:list[dict] = connectToMySQL(db).query_db(query)
+        event_objects:list[Event] = []
+        for event in results:
+            event_objects.append(cls(event))
+        print([obj.user_id for obj in event_objects])
+        return event_objects
+
+    @classmethod
+    def create_valid_event(cls, event_dict):
+        if not cls.is_valid(event_dict):
+            return False
+        query = "INSERT INTO events (name, date, time, location, details, created_at, updated_at) VALUES (%(name)s, %(game)s, %(date)s, %(time)s, %(location)s, %(details)s, NOW(), NOW());"
+        event_id = connectToMySQL(db).query_db(query, event_dict)
+        print(event_id)
+        event = cls.get_by_id(event_id)
+        return event
+
+    @classmethod
+    def get_by_id(cls, event_id):
+        print(f"get event by id {event_id}")
+        data = {"id": event_id}
+        query = """SELECT events.id as event_id, events.created_at, events.updated_at, name, date, time, location, users.id as user_id, num_applied, num_of_users, details, first_name, last_name, email, password, users.created_at as uc, users.updated_at as uu FROM events LEFT JOIN users_events ON events.id = users_events.event_id LEFT JOIN users ON users.id = users_events.user_id WHERE events.id = %(id)s;"""
+        result = connectToMySQL(db).query_db(query,data)
+        result = result[0]
+        event = cls(result)
+        print(event)
+        event.user = User.User(
+            {
+                    "id": result["user_id"],
+                    "first_name": result["first_name"],
+                    "last_name": result["last_name"],
+                    "email": result["email"],
+                    "password": result["password"],
+                    "created_at": result["uc"],
+                    "updated_at": result["uu"]
+            }
+        )
+        print(event.user)
+        return event
+
+    @classmethod
+    def delete_event_by_id(cls, event_id):
+        data = {"id": event_id}
+        query = "DELETE from events WHERE id = %(id)s;"
+        connectToMySQL(db).query_db(query,data)
+        return event_id
+
+    @classmethod
+    def join_event(cls, event_dict):
+        print(event_dict)
+        query = "INSERT INTO users_events (user_id, event_id) VALUES (%(user_id)s, %(event_id)s);"
+        connectToMySQL(db).query_db(query, event_dict)
+        query = "UPDATE events SET num_applied = num_applied + 1 WHERE id = %(event_id)s;"
+        connectToMySQL(db).query_db(query, event_dict)
+
+    @classmethod
+    def leave_event(cls, event_dict):
+        print(event_dict)
+        query = "DELETE FROM users_events WHERE user_id = %(user_id)s AND event_id = %(event_id)s"
+        connectToMySQL(db).query_db(query, event_dict)
+        query = "UPDATE events SET num_applied = num_applied - 1 WHERE id = %(event_id)s;"
+        connectToMySQL(db).query_db(query, event_dict)
+
+    @staticmethod
+    def is_valid(event_dict):
+        valid = True
+        if len(event_dict["name"]) < 2:
+            flash("Name has to be at least 2 characters long.")
+            valid = False
+        if len(event_dict["location"]) < 8:
+            flash("Location has to be at least 8 characters long.")
+            valid = False
+        if len(event_dict["details"]) < 10:
+            flash("Details have to be at least 10 characters long.")
+            valid = False
+        return valid
---
+++ flask_app/models/User.py
@@ -0,0 +1,141 @@
+from sqlite3 import connect
+from flask_app import app
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+from flask_bcrypt import Bcrypt
+from flask_app.models import Event
+import re
+
+bcrypt = Bcrypt(app)
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+
+db = "PetsOnly"
+
+class User:
+    def __init__(self, data:dict):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def get_all_users(cls):
+        query = "SELECT * FROM users;"
+        users_data:list[dict] = connectToMySQL(db).query_db(query)
+        user_objects:list[User] = []
+        for user in users_data:
+            user_objects.append(cls(user))
+
+        return user_objects
+    
+
+    @classmethod
+    def get_by_email(cls,email):
+
+        data = {
+            "email" : email
+        }
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        result = connectToMySQL(db).query_db(query,data)
+        if len(result) < 1:
+            return False
+        return cls(result[0])
+
+    @classmethod
+    def get_by_id(cls, user_id):
+        data = {"id" : user_id}
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        result = connectToMySQL(db).query_db(query,data)
+        if len(result) < 1:
+            return False
+        return cls(result[0])
+
+    @classmethod
+    def update_user_account(cls, user_dict, session_id):
+        if not cls.valid_user_update(user_dict):
+            return False
+        query = "UPDATE users SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s WHERE id = %(id)s;"
+        user = connectToMySQL(db).query_db(query, user_dict)
+        return user
+
+    @classmethod
+    def create_user(cls,user):
+        if not cls.validate_user(user):
+            return False
+        pw_hash = bcrypt.generate_password_hash(user['password'])
+        user = user.copy()
+        user['password'] = pw_hash
+        query = "INSERT INTO users (first_name, last_name, email, password, created_at, updated_at) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s, NOW(), NOW());"
+        new_user_id = connectToMySQL(db).query_db(query, user)
+        new_user = cls.get_by_id(new_user_id)
+        return new_user
+
+    @classmethod
+    def user_login_authentication(cls, user_input):
+        valid = True
+        existing_user = cls.get_by_email(user_input["email"])
+        password_valid = True
+        if not existing_user:
+            valid = False
+        else:
+            password_valid = bcrypt.check_password_hash(existing_user.password, user_input['password'])
+            if not password_valid:
+                valid = False
+        if not valid:
+            flash("Your credentials do no match our records.")
+            return False
+        return existing_user
+
+    @staticmethod
+    def validate_user(user):
+        is_valid = True
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        result = connectToMySQL(db).query_db(query,user)
+        if len(result) >= 1:
+            flash("Email already in use. Please Log In.")
+            is_valid=False
+        if not EMAIL_REGEX.match(user['email']):
+            flash("Invalid email address!")
+            is_valid = False
+        if len(user['first_name']) < 3:
+            flash("First name must be at least 3 characters.")
+            is_valid = False
+        if len(user['last_name']) < 3:
+            flash("Last Name must be at least 3 characters.")
+            is_valid = False
+        if len(user['email']) < 5:
+            flash("Email address must be at least 5 characters.")
+            is_valid = False
+        if len(user['password']) < 8:
+            flash("Password must be at least 8 characters long")
+            is_valid = False
+        if user['password'] != user['confirm_password']:
+            flash("Passwords do not match.")
+            is_valid = False
+        return is_valid
+
+    @staticmethod
+    def valid_user_update(user):
+        is_valid = True
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        result = connectToMySQL(db).query_db(query,user)
+        if len(result) >= 1:
+            flash("Email already in use. Try a different email.")
+            is_valid=False
+        if not EMAIL_REGEX.match(user['email']):
+            flash("Invalid email address!")
+            is_valid = False
+        if len(user['first_name']) < 3:
+            flash("First name must be at least 3 characters.")
+            is_valid = False
+        if len(user['last_name']) < 3:
+            flash("Last Name must be at least 3 characters.")
+            is_valid = False
+        if len(user['email']) < 5:
+            flash("Email address must be at least 5 characters.")
+            is_valid = False
+        return is_valid
