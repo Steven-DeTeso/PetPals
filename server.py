46f92a041323757c1efb0e917759d82a1b0b6ca8
Steven DeTeso <sadeteso@gmail.com>
2022-12-21 14:59:18 -0500

    just updated some routes, only 10mins of work

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask_app import app
-from flask_app.controllers import users
+from flask_app.controllers import users, events, friends, statuses
 # won't be highlighted because we won't use it here, it will just be active
 
 if __name__=="__main__":
